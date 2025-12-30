d5205fbe9bb0714612a434215f5e62696f73516a
tanmay-ign <tanmayupadhyay2005@gmail.com>
2025-12-30 13:08:02 +0530

    Delete src directory

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as LiveFeed } from './LiveFeed';
-export { default as ThreatBadge } from './ThreatBadge';
-export { default as AlertsPanel } from './AlertsPanel';
-export { default as Dashboard } from './Dashboard';
-export { default as Navbar } from './Navbar';
-export { default as SystemStatus } from './SystemStatus';
